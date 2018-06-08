7cc57e5c5c4c38ac5609435b10dbd292de164dca
hirozak <shrak0224@gmail.com>
2018-06-08 18:48:35 +0900

    edit devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
+
+  before_action :basic, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :basic
 
   protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
 
   def basic
     authenticate_or_request_with_http_basic do |user, pass|
       user == ENV["BASIC_USER"] && pass = ENV["BASIC_PASS"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
