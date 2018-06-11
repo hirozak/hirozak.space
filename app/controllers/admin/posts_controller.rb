c99812ed501f7355dc23b44d946af3af45b2086a
hirozak <shrak0224@gmail.com>
2018-06-11 21:00:10 +0900

    create posts new function

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,27 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_user?
+
   def index
   end
 
   def new
+    @post = current_user.posts.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.category_id = 1
+    @post.save!
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :image)
+  end
+
+  def admin_user?
+    redirect_to root_path if !current_user.admin?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :admin, only: %i(index)
   namespace :admin do
-    resources :posts, only: %i(index new)
+    resources :posts, only: %i(index new create)
   end
   root 'top#index'
 end
