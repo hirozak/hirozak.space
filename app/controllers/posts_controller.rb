3f79179dfcd9a83ca3c3ace49bd71761328f97ed
hirozak <shrak0224@gmail.com>
2018-06-24 23:17:14 +0900

    edit post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.published.find_by(slug: params[:id])
     @popular_posts = Post.published.favorite.includes(:category).default_order.limit(5)
-    @similar_posts = @post.category.posts.published.includes(:category).default_order.limit(4)
+    @similar_posts = @post.category.posts.published.includes(:category).where("id != ?", @post.id).default_order.limit(4)
   end
 
 end
