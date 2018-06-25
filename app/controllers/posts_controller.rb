b06ccf4d6f123843425e6da31aafc99cee69fa5f
hirozak <shrak0224@gmail.com>
2018-06-26 06:47:50 +0900

    fix issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.published.find_by(slug: params[:id])
-    @popular_posts = Post.published.favorite.includes(:category).default_order.limit(5)
+    @popular_posts = Post.published.favorite.includes(:category).limit(5)
     @similar_posts = @post.category.posts.published.includes(:category).where("id != ?", @post.id).default_order.limit(4)
   end
 
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ class TopController < ApplicationController
 
   def index
     @posts = Post.published.includes(:category).default_order.limit(8)
-    @popular_posts = Post.published.favorite.includes(:category).default_order.limit(4)
+    @popular_posts = Post.published.favorite.includes(:category).limit(4)
     @slider_posts = Post.published.favorite.includes(:category).default_order.limit(8)
   end
 
