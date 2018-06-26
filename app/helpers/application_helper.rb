492ea75faee3f753f7ff820ba2f9d5d633b683a0
hirozak <shrak0224@gmail.com>
2018-06-26 13:09:16 +0900

    fix issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,5 @@
 module ApplicationHelper
 
-  def full_title(page_title='')
-    base_title = "hirozak"
-    if page_title.empty?
-      base_title
-    else
-      page_title + " | " + base_title
-    end
-  end
-
   def default_meta_tags
     {
       site: 'hirozak',
