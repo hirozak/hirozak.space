df2eb36f295c95e119ebcf922a7b2e3c82676e03
hirozak <shrak0224@gmail.com>
2019-03-01 18:03:55 +0900

    Change design admin posts

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -40,15 +40,15 @@ class Admin::PostsController < ApplicationController
   end
 
   def all
-    @posts = current_user.posts.includes(:category).default_order.page(params[:page]).per(8)
+    @posts = current_user.posts.includes(:category).default_order.page(params[:page]).per(10)
   end
 
   def published
-    @posts = current_user.posts.published.includes(:category).default_order.page(params[:page]).per(8)
+    @posts = current_user.posts.published.includes(:category).default_order.page(params[:page]).per(10)
   end
 
   def drafts
-    @posts = current_user.posts.drafts.includes(:category).default_order.page(params[:page]).per(8)
+    @posts = current_user.posts.drafts.includes(:category).default_order.page(params[:page]).per(10)
   end
 
   private
