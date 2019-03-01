49ac0320326b24d4aac2c6d800f51a2d87ad97a8
hirozak <shrak0224@gmail.com>
2019-03-01 17:30:23 +0900

    Add pagination to top

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.published.includes(:category).default_order.page(params[:page]).per(8)
-    @slider_posts = Post.published.favorite.includes(:category).default_order.limit(8)
+    @slider_posts = Post.published.favorite.default_order.limit(8)
   end
 
   def show
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @posts = Post.published.includes(:category).default_order.limit(8)
+    @posts = Post.published.includes(:category).default_order.page(params[:page]).per(8)
     @popular_posts = PopularPost.includes(post: :category).map(&:post).take(4)
     @slider_posts = Post.published.favorite.includes(:category).default_order.limit(8)
   end
