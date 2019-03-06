51efdde55079224aaeaea71033ea8d24e2d907dc
hirozak <shrak0224@gmail.com>
2019-03-06 20:57:45 +0900

    Fix post previous and next

--- app/models/post.rb
+++ app/models/post.rb
@@ -25,10 +25,10 @@ class Post < ApplicationRecord
   end
 
   def previous
-    Post.published.default_order.find_by('created_at <= ? and id < ?', created_at, id)
+    Post.published.default_order.find_by('created_at < ?', created_at)
   end
 
   def next
-    Post.published.default_order.where('created_at >= ? and id > ?', created_at, id).reverse.first
+    Post.published.default_order.where('created_at > ?', created_at).reverse.first
   end
 end
